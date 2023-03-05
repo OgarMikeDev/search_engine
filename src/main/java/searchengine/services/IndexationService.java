9df8d095bdf2a172565ea9ced32ef4ff8ee3983b
Mike Ogar <ogar.m.v@mail.ru>
2023-03-05 14:01:35 +0300

    "Search Engine"

---
+++ src/main/java/searchengine/services/IndexationService.java
@@ -0,0 +1,15 @@
+package searchengine.services;
+
+import java.util.concurrent.RecursiveAction;
+
+public class IndexationService extends RecursiveAction {
+
+    private void startIndexing() {
+
+    }
+
+    @Override
+    protected void compute() {
+
+    }
+}
