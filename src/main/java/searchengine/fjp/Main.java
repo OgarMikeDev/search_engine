cd4c2b1cddc98e2d143e4102be8eff6ce6cb48ae
Mike Ogar <ogar.m.v@mail.ru>
2023-03-11 13:03:18 +0300

    "Search Engine"

--- src/main/java/searchengine/fjp/Main.java
+++ src/main/java/searchengine/fjp/Main.java
@@ -2,34 +2,42 @@ package searchengine.fjp;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import searchengine.model.Enum;
+import searchengine.model.Site;
 import searchengine.repositories.PageRepository;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.atomic.AtomicInteger;
+
 @RequiredArgsConstructor
 public class Main {
     public static String url = "http://www.playback.ru/";
-//    public static PrintWriter writer;
+    private static AtomicInteger idGenerator = new AtomicInteger();
 
     public static void main(String[] args) {
         UrlsContainer.setMainPageUrl(url);
+
         Node node = new ForkJoinPool()
                 .invoke(new WebScraper(new Node(url)));
-//        try {
-//            writer = new PrintWriter("data/siteMap.txt");
-//            List<String> urlsList = node.getAllUrls();
-//            for(String urls : urlsList){
-//                writer.write(urls);
-//
-//            }
-//            writer.flush();
-//            writer.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+
+        int idSite = generatedId();
+        Site site = new Site();
+        site.setId(idSite);
+        site.setStatus(Enum.INDEXING);
+        site.setStatusTime(LocalDateTime.now());
+        site.setLastError("");
+        site.setUrl(url);
+        site.setName(url);
+    }
+
+
+    private static int generatedId() {
+        return idGenerator.getAndIncrement();
     }
 }
