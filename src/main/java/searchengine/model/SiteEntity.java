c6ee40d85cb3ae1ea99c72b933a04fde6c6cd884
Mike Ogar <ogar.m.v@mail.ru>
2023-03-26 14:20:22 +0300

    "Search Engine"

--- src/main/java/searchengine/model/SiteEntity.java
+++ src/main/java/searchengine/model/SiteEntity.java
@@ -13,7 +13,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 @NoArgsConstructor
 public class SiteEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     private int id;
 
